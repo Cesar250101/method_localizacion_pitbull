ae33d01a3159fa95c13cfad77672d2d34f2d73b2
Your Name <you@example.com>
2021-10-06 11:33:56 -0300

    Traspasa notas a la guía de despacho

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     """,
 
     'author': "Method ERP",
-    'website': "http://www.ymethod.cl",
+    'website': "http://www.method.cl",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','purchase','account'],
+    'depends': ['base','purchase','account','sale'],
 
     # always loaded
     'data': [
--- models/models.py
+++ models/models.py
@@ -2,14 +2,17 @@
 
 from odoo import models, fields, api
 
-# class method_localizacion_pitbull(models.Model):
-#     _name = 'method_localizacion_pitbull.method_localizacion_pitbull'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class NotasVenta(models.Model):
+    _inherit = 'sale.order'
+    
+    notas_guia = fields.Text(string='Notas Guia Despacho',help='Este texto pasara al campo NOTAS de la guía de despacho')
+    
+    
+    @api.multi
+    def action_confirm(self):
+        if self.notas_guia:
+            super(NotasVenta, self).action_confirm()
+            guia=self.env['stock.picking'].search([('sale_id','=',self.id)],limit=1)
+            guia.note=self.notas_guia
+        
