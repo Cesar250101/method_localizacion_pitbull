d62c19528363c628d38dfd9a6d2e9ed4385173f7
Cesar250101 <cesar250101@outlook.com>
2022-05-17 12:50:45 -0400

    Datos de CRm a Sale Order

--- models/models.py
+++ models/models.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from operator import truth
+
+from numpy import True_
 from odoo import models, fields, api
 
 
@@ -7,6 +10,8 @@ class NotasVenta(models.Model):
     _inherit = 'sale.order'
     
     notas_guia = fields.Text(string='Notas Guia Despacho',help='Este texto pasara al campo NOTAS de la guía de despacho')
+    crm_producto=fields.Char(string='Producto CMR')
+    crm_medio=fields.Char(string='Medio CMR')
     
     
     @api.multi
@@ -15,4 +20,11 @@ class NotasVenta(models.Model):
         if self.notas_guia:
             guia=self.env['stock.picking'].search([('sale_id','=',self.id)],limit=1)
             guia.note=self.notas_guia
+
+    @api.model
+    def actualizar_datos_crm(self):
+        self.crm_producto=self.opportunity_id.x_producto
+        self.crm_medio=self.opportunity_id.x_medio
+        return True
+
         
