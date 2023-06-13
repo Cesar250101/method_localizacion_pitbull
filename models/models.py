e3126d7cf96cea582388dec70a8cc275d2d907ff
Cesar250101 <Cesar250101@outlook.com>
2023-06-13 13:39:26 -0400

    Notificación Cambio de vendedor

--- models/models.py
+++ models/models.py
@@ -4,7 +4,50 @@ from operator import truth
 
 from numpy import True_
 from odoo import models, fields, api
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
+
+class Partner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.multi
+    def notificar_cambio_vendedor(self):
+        # Configuración del servidor SMTP
+        servidor=self.env['ir.mail_server'].search([('smtp_user','=','noreply@method.cl')],limit=1)
+        smtp_host = servidor.smtp_host
+        smtp_port = servidor.smtp_port
+        smtp_username = servidor.smtp_user
+        smtp_password = servidor.smtp_pass
+
+        # Crear objeto de mensaje
+        mensaje = MIMEMultipart()
+        mensaje['From'] = 'Method ERP'
+        mensaje['To'] = 'nicolas@pitbull.cl,cesar@method.cl'
+        mensaje['Subject'] = 'Cambio de vendedor'
+
+        # Cuerpo del mensaje
+        cuerpo_mensaje = """
+            Estimado/a Pitbull Sociedad Anonima
+
+            Se ha cambiado el vendedor al contacto """ +  self.name+""" , el nuevo vendedor es """ + self.user_id.name +""".
+
+            Saludos.
+        """
+        mensaje.attach(MIMEText(cuerpo_mensaje, 'plain'))
+
+        # Establecer conexión segura con el servidor SMTP utilizando SSL/TLS
+        smtp = smtplib.SMTP_SSL(smtp_host, smtp_port)
+
+        # Iniciar sesión en el servidor SMTP
+        smtp.login(smtp_username, smtp_password)
+
+        # Enviar correo electrónico
+        smtp.send_message(mensaje)
+
+        # Cerrar conexión con el servidor SMTP
+        smtp.quit()
 
 class NotasVenta(models.Model):
     _inherit = 'sale.order'
